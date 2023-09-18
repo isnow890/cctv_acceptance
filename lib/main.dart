5805333371c8623d77d4863dbf579120b99365b1
“isnow890” <isnow890@gmail.com>
2023-09-18 22:18:22 +0900

    복구

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:go_router/go_router.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:url_strategy/url_strategy.dart'; // 패키지를 import.
-import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 import 'router/provider/go_router_provider.dart';
 import 'theme/component/constrained_screen.dart';
 import 'theme/provider/theme_provider.dart';
 
 void main() async {
-
+  // setPathUrlStrategy();
 
   runApp(const ProviderScope(child: _App()));
 }
@@ -35,21 +33,24 @@ class _App extends ConsumerWidget {
             ref.read(themeServiceProvider.notifier).toggleTheme();
           }
 
-          return MaterialApp.router(
-            useInheritedMediaQuery: true,
-            title: '다양한 응답 기록',
-            builder: (context, child) {
-              return Overlay(
-                initialEntries: [
-                  OverlayEntry(
-                    builder: (context) => child!,
-                  )
-                ],
-              );
-            },
-            routerConfig: router,
-            theme: ref.read(themeServiceProvider.notifier).themeData,
-            debugShowCheckedModeBanner: false,
+          return Center(
+            child: ConstrainedScreen(
+              child: MaterialApp.router(
+                title: '다양한 응답 기록',
+                builder: (context, child) {
+                  return Overlay(
+                    initialEntries: [
+                      OverlayEntry(
+                        builder: (context) => child!,
+                      )
+                    ],
+                  );
+                },
+                routerConfig: router,
+                theme: ref.read(themeServiceProvider.notifier).themeData,
+                debugShowCheckedModeBanner: false,
+              ),
+            ),
           );
         });
   }
